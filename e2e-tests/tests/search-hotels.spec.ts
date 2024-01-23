61b3946a0f5d33aef9e596c1984f3a5ca6885fa6
kike-pinto <cripintort@gmail.com>
2024-01-23 17:44:20 -0300

    adding my bookings

--- e2e-tests/tests/search-hotels.spec.ts
+++ e2e-tests/tests/search-hotels.spec.ts
@@ -44,30 +44,37 @@ test('should show hotel detail', async ({ page }) => {
 })
 
 test('should book hotel', async ({ page }) => {
-  await page.goto(UI_URL)
+  try {
+    await page.goto(UI_URL)
 
-  await page.getByPlaceholder('Where are you going?').fill('Dublin')
+    await page.getByPlaceholder('Where are you going?').fill('Dublin')
 
-  const date = new Date()
-  date.setDate(date.getDate() + 3)
-  const formattedDate = date.toISOString().split('T')[0]
-  await page.getByPlaceholder('Check-out Date').fill(formattedDate)
+    const date = new Date()
+    date.setDate(date.getDate() + 3)
+    const formattedDate = date.toISOString().split('T')[0]
+    await page.getByPlaceholder('Check-out Date').fill(formattedDate)
 
-  await page.getByRole('button', { name: 'Search' }).click()
+    await page.getByRole('button', { name: 'Search' }).click()
 
-  await page.getByText('Dublin Getaways').click()
-  await page.getByRole('button', { name: 'Book now' }).click()
+    await page.getByText('Dublin Getaways').click()
+    await page.getByRole('button', { name: 'Book now' }).click()
 
-  await expect(page.getByText('Total Cost: $300.00')).toBeVisible()
+    await expect(page.getByText('Total Cost: $300.00')).toBeVisible()
 
-  const stripeFrame = page.frameLocator('iframe').first()
-  await stripeFrame
-    .locator('[placeholder="Card number"]')
-    .fill('4242424242424242')
-  await stripeFrame.locator('[placeholder="MM / YY"]').fill('04/30')
-  await stripeFrame.locator('[placeholder="CVC"]').fill('242')
-  await stripeFrame.locator('[placeholder="ZIP"]').fill('24242')
+    const stripeFrame = page.frameLocator('iframe').first()
+    await stripeFrame
+      .locator('[placeholder="Card number"]')
+      .fill('4242424242424242')
+    await stripeFrame.locator('[placeholder="MM / YY"]').fill('04/30')
+    await stripeFrame.locator('[placeholder="CVC"]').fill('242')
+    await stripeFrame.locator('[placeholder="ZIP"]').fill('24242')
 
-  await page.getByRole('button', { name: 'Confirm Booking' }).click()
-  await expect(page.getByText('Booking saved!')).toBeVisible()
+    await page.getByRole('button', { name: 'Confirm Booking' }).click()
+    await expect(page.getByText('Booking saved!')).toBeVisible()
+
+    await page.getByRole('link', { name: 'My Bookings' }).click()
+    await expect(page.getByText('Dublin Gataways')).toBeVisible()
+  } catch (error) {
+    console.log('error', error)
+  }
 })
